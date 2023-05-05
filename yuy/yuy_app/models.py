f7c73abba86901770d2633e51de346f34e1004bf
MFW <gleb.rogachev.05@mail.ru>
2023-05-05 22:25:32 +0300

    backend already done

--- yuy/yuy_app/models.py
+++ yuy/yuy_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    text = models.TextField()
+    photo = models.ImageField(upload_to=r'static\img\photo', height_field=None, width_field=None)
+
+
+    def __str__(self):
+        return self.name
+    
+
+    class Meta:
+        verbose_name='Member'
+        verbose_name_plural='Members'
--- yuy/yuy_app/views.py
+++ yuy/yuy_app/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Member
+
 # Create your views here.
 
 
 def index(request):
 
+    list_members = Member.objects.all()
+
+    print(list_members)
+
     context = {
+        'list_members' : list_members
     }
 
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
+
+
+def member(request, id):
+
+    get_member = Member.objects.get(id=id)
+
+    context = {
+        'member' : get_member,
+    }
+
+    return render(request, 'member.html', context)
